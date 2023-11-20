53075e57ca35e968be4983971b30f95e48bf7aad
davinanurul <davinanurul06@gmail.com>
2023-11-20 11:09:19 +0700

    landing page

---
+++ index.php
@@ -0,0 +1,100 @@
+<!-- baru sampai bagian ambil data produk dari database -->
+
+<?php
+    // require "koneksi.php";
+    // $queryProduk = mysqli_query($con, "SELECT id, nama, harga, foto, detail FROM produk LIMIT 6");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Home</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <?php require "navbar.php"; ?>
+
+    <!-- banner -->
+    <div class="container-fluid banner d-flex align-items-center">
+        <div class="container text-center text-white">
+            <h1>Toko Online Fashion</h1>
+            <h3>Mau Cari Apa?</h3>
+           <div class="col-md-8 offset-md-2">
+            <form method="get" action="produk.php">
+                <div class="input-group input-grouop-lg my-4">
+                    <input type="text" class="form-control" placeholder="Nama Barang" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <button type="submit" class="btn warna2 text-white">Telusuri</button>
+                 </div>
+            </forrm>
+           </div>
+        </div>
+    </div>
+
+    <!-- highlighted kategori -->
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3>kategori Terlaris</h3>
+            <div class="row mt-5">
+                <div class="col-md-4 mb-3">
+                    <div class="highlighted-kategori kategori-baju-pria d-flex justify-content-center
+                    align-items-center">
+                        <h4><a href="produk.php?kategori=baju pria">Baju Pria</a></h4>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="highlighted-kategori kategori-baju-wanita d-flex justify-content-center
+                    align-items-center">
+                    <h4><a href="produk.php?kategori=baju wanita">Baju Wanita</a></h4>
+                </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="highlighted-kategori kategori-sepatu d-flex justify-content-center
+                    align-items-center">
+                    <h4><a href="produk.php?kategori=sepatu">Sepatu</a></h4>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- tentang kami -->
+    <div class="container-fluid warna3 py-5">
+        <div class="container text-center">
+            <h3>Tentang Kami</h3>
+            <p class="fs-5 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sunt fugiat ab praesentium omnis nemo veniam exercitationem iure vero eos ipsam, numquam distinctio doloremque natus excepturi, placeat voluptatibus dignissimos dolorum.</p>
+        </div>
+    </div>
+
+    <!-- produk -->
+    <div class="container-fluid py-5">
+        <div class="container text-center">
+            <h3>Produk</h3>
+
+            <div class="row mt-5">
+                <?php while($data = mysqli_fetch_array($queryProduk)){ ?>
+                <div class="col=sm-6 col-md-4 mb-3">
+                    <div class="card">
+                        <img src="image/<?php echo $data['foto']; ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $data['nama']; ?></h5>
+                            <p class="card-text text-truncate"><?php echo $data['detail']; ?></p>
+                            <p class="card-text text-harga">Rp. <?php echo $data['harga']; ?></p>
+                            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 text-white">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js.all.min.js"></script>
+</body>
+</html>

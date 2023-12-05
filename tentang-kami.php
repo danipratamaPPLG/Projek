7abf39bba0960788eba696982e3db0382bc507b9
danipratamaPPLG <danidonidino@gmail.com>
2023-12-05 21:38:06 +0700

    landing page dan detail produk page dan katalog

---
+++ tentang-kami.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "navbar.php"; ?>
+
+    <!-- banner -->
+    <div class="container-fluid banner2 d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <!-- main -->
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum repellendus cumque, quibusdam laboriosam totam magni esse perferendis magnam, voluptates cupiditate minima itaque incidunt distinctio consectetur! Modi commodi atque, consequuntur ad quae eius pariatur quos fugiat nemo expedita iure sunt recusandae laudantium qui aperiam culpa architecto nam maxime quas. Officiis perspiciatis expedita voluptatibus quaerat fugit quidem, maxime, voluptate illo iusto totam quis, nobis cum iure magni fuga id eum est aspernatur. At molestias, adipisci labore non quae quo obcaecati officia culpa impedit praesentium et earum accusantium ullam? Fugit iure assumenda facere molestiae mollitia dolore officiis, sequi quasi, maiores nisi, provident illum!
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam veniam aliquid voluptatem repellat tenetur incidunt voluptas vitae sequi explicabo expedita provident alias facere culpa dicta, ipsa harum totam illo? Impedit itaque ipsa iure fugiat cumque nostrum ducimus, eveniet explicabo porro. Quasi est natus labore quisquam officiis hic assumenda aut corrupti id officia voluptas quibusdam tenetur inventore unde, nobis dolore rem illum delectus fugit sapiente similique? Repellendus magnam eveniet recusandae vel id asperiores sint dicta aperiam, quos ipsa expedita nesciunt ex?
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum inventore recusandae, nesciunt labore dolore autem at architecto eveniet dolorem, perspiciatis eligendi quisquam. Reiciendis tempore ipsam quia quis saepe iusto nesciunt quisquam veritatis aperiam vel? Ea dignissimos eius atque iure, voluptatum quo itaque, fugiat natus, velit iste corporis error reiciendis officia quisquam hic veniam repudiandae eaque. Sapiente adipisci ab est sint quam, quod facere voluptatem maiores atque delectus provident repellendus ea neque voluptatibus? Fuga, minus voluptatibus est atque et iste incidunt quasi velit ratione quos adipisci nostrum officia corrupti omnis praesentium quae in cumque nihil provident, culpa quisquam accusamus tempora dolorem consectetur. Nemo aspernatur ipsa soluta omnis officiis. Adipisci nemo ex quas assumenda laboriosam, molestias velit itaque, error repellendus nobis labore quam nesciunt et quo sequi ducimus doloribus, accusantium repudiandae beatae amet consectetur eveniet. Ut placeat earum corrupti explicabo rerum eum incidunt laudantium commodi, debitis, nostrum quam itaque consequuntur quia doloribus?
+            </p>
+        </div>
+    </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+</html>
